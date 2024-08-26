7c51aa778b65d36c8a0f297e43f6f5de5513612a
jossebv <josebravopacheco@gmail.com>
2024-08-26 18:22:01 +0200

    add: implement LoRA version of the model

--- src/models_definitions/TinyImageQA.py
+++ src/models_definitions/TinyImageQA.py
@@ -1,10 +1,9 @@
-from transformers import (
-    AutoModelForSeq2SeqLM,
-    AutoModel,
-)
+from transformers import AutoModelForSeq2SeqLM, AutoModel
 import torch
 from torch import nn
 import lightning as L
+import LoRA as lora
+from functools import partial
 
 
 class TinyImageQA(L.LightningModule):
@@ -85,6 +84,85 @@ class TinyImageQA(L.LightningModule):
         return optimizer
 
 
+class TinyImageQAWLoRA(L.LightningModule):
+    def __init__(
+        self,
+        lora_r,
+        lora_alpha,
+        lora_dropout,
+        lora_query=True,
+        lora_key=False,
+        lora_value=True,
+        lora_projection=False,
+        lora_mlp=False,
+        lr=1e-4,
+    ):
+        super().__init__()
+
+        assign_lora = partial(lora.LinearWithLoRA, rank=lora_r, alpha=lora_alpha)
+        self.tinyImage = TinyImageQA(lr=lr)
+        for param in self.tinyImage.parameters():
+            param.requires_grad = False
+
+        for block in self.tinyImage.llm.decoder.block:
+            if lora_query:
+                block.layer[0].SelfAttention.q = assign_lora(
+                    block.layer[0].SelfAttention.q
+                )
+                block.layer[1].EncDecAttention.q = assign_lora(
+                    block.layer[1].EncDecAttention.q
+                )
+            if lora_key:
+                block.layer[0].SelfAttention.k = assign_lora(
+                    block.layer[0].SelfAttention.k
+                )
+                block.layer[1].EncDecAttention.k = assign_lora(
+                    block.layer[1].EncDecAttention.k
+                )
+            if lora_value:
+                block.layer[0].SelfAttention.v = assign_lora(
+                    block.layer[0].SelfAttention.v
+                )
+                block.layer[1].EncDecAttention.v = assign_lora(
+                    block.layer[1].EncDecAttention.v
+                )
+            if lora_projection:
+                block.layer[0].SelfAttention.o = assign_lora(
+                    block.layer[0].SelfAttention.o
+                )
+                block.layer[1].EncDecAttention.o = assign_lora(
+                    block.layer[1].EncDecAttention.o
+                )
+            if lora_mlp:
+                block.layer[2].DenseReluDense.wi_0 = assign_lora(
+                    block.layer[2].DenseReluDense.wi_0
+                )
+                block.layer[2].DenseReluDense.wi_1 = assign_lora(
+                    block.layer[2].DenseReluDense.wi_1
+                )
+                block.layer[2].DenseReluDense.wo = assign_lora(
+                    block.layer[2].DenseReluDense.wo
+                )
+
+    def forward(self, image, prompt_ids, prompt_att_masks, target_ids):
+        logits, loss = self.tinyImage.forward(
+            image, prompt_ids, prompt_att_masks, target_ids
+        )
+        return logits, loss
+
+    def training_step(self, batch, batch_idx):
+        image, prompt_ids, prompt_att_masks, target_ids = batch
+        logits, loss = self(image, prompt_ids, prompt_att_masks, target_ids)
+
+        self.log("train/loss", loss)
+
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
+        return optimizer
+
+
 if __name__ == "__main__":
     import sys
     from transformers import AutoTokenizer
@@ -92,10 +170,12 @@ if __name__ == "__main__":
     sys.path.append(".")
     from src.data_module import RLAIFDataModule
 
+    model = TinyImageQAWLoRA(lora_r=4, lora_alpha=16, lora_dropout=0.05)
+    print(model.tinyImage.llm.decoder)
+
     train_dataloader = RLAIFDataModule(batch_size=4).train_dataloader()
     batch = next(iter(train_dataloader))
 
-    model = TinyImageQA()
     output_logits, loss = model(*batch)
     print(loss)
 
